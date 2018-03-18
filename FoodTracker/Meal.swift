8909160b894891bf0efad4445f2891f2175d362d
Taras Didukh <tdidukh@iMac-Taras.local>
2018-03-18 21:58:31 +0200

    Finished <Define You Data Model> step

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,35 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Taras Didukh on 3/18/18.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
+
